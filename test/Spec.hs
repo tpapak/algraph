1e8b5816192c2709ac8b1659c7d26bc0e4824221
Thodoris Papakonstantinou <dev@tpapak.com>
2026-02-27 13:00:21 +0200

    Remove old implementation and comparison tests
    PureOld and OldVsNew served to verify the refactoring produced
    identical results (1000 random graphs). No longer needed.

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,7 +6,6 @@ import Test.Graph.AdjacencyList.DFS as DFS
 import Test.Graph.AdjacencyList.WFI as WFI
 import Test.Graph.AdjacencyList.Metrics as Met
 import Test.Graph.AdjacencyList.PushRelabel.Pure as PRP
-import Test.Graph.AdjacencyList.PushRelabel.OldVsNew as OldVsNew
 
 main :: IO ()
 main = do
@@ -22,6 +21,3 @@ main = do
       ++ Met.fastTests
   T.reportTestsIO
     Met.ioTests
-  putStrLn "\nQuickCheck Property Tests"
-  T.reportTestsIO
-    OldVsNew.ioTests
